f3c49d6a262d9c996303cbdd55ee5c5874953db6
muradisgandar <mr.murad157@mail.ru>
2020-02-20 11:18:00 +0400

    post,put,delete request with ajax

--- udemy/Mustafa Murat Coshkun/Asynchronous programming , AJAX , before ES6/GET,POST,UPDATE,DELETE with AJAX/app.js
+++ udemy/Mustafa Murat Coshkun/Asynchronous programming , AJAX , before ES6/GET,POST,UPDATE,DELETE with AJAX/app.js
@@ -1,16 +1,17 @@
 // Ajax,callback , http requests
 
-class Request{
+class Request {
 
-    constructor(){
+    constructor() {
         this.xhr = new XMLHttpRequest();
     }
 
     // Get request
-    get(url,callback){
+    get(url, callback) {
 
-        this.xhr.open("GET",url);
-        
+
+        this.xhr.open("GET", url);
+
 
         // this.xhr.onload = function(){
         //     if(this.xhr.status === 200){
@@ -22,13 +23,13 @@ class Request{
 
 
         // bind evezine arrow function da istifade etmek olar
-        this.xhr.onload = () =>{
-            if(this.xhr.status === 200){
-                callback(null,this.xhr.responseText);
+        this.xhr.onload = () => {
+            if (this.xhr.status === 200) {
+                callback(null, this.xhr.responseText);
             }
-            else{
+            else {
                 // Error
-                callback("Error",null);
+                callback("Error", null);
             }
 
         };
@@ -36,10 +37,85 @@ class Request{
 
         this.xhr.send();
     }
+
+    post(url, data, callback) {
+        this.xhr.open("POST", url);
+        this.xhr.setRequestHeader("Content-type", "application/json");
+
+
+        this.xhr.onload = () => {
+            if (this.xhr.status === 201) {
+                // Successfully
+                callback(null, this.xhr.responseText);
+            }
+            else {
+                callback("Error...", null);
+            }
+        }
+
+
+        this.xhr.send(JSON.stringify(data));
+
+    }
+
+    put(url, data, callback) {
+        this.xhr.open("PUT", url);
+        this.xhr.setRequestHeader("Content-type", "application/json");
+
+
+        this.xhr.onload = () => {
+            if (this.xhr.status === 200) {
+                // Successfully
+                callback(null, this.xhr.responseText);
+            }
+            else {
+                callback("Error...", null);
+            }
+        }
+
+
+        this.xhr.send(JSON.stringify(data));
+
+    }
+
+    delete(url, callback) {
+
+
+        this.xhr.open("DELETE", url);
+
+        this.xhr.onload = () => {
+            if (this.xhr.status === 200) {
+                callback(null, "Successfully deleted");
+            }
+            else {
+                // Error
+                callback("Error", null);
+            }
+
+        };
+
+
+        this.xhr.send();
+    }
+
 }
 const request = new Request();
 
 // request.get("https://jsonplaceholder.typicode.com/albums",function(err,response){
+//     if(err === null){
+//         //Successfully
+//         console.log(response);    // it is string form
+//         // console.log(JSON.parse(response));  // it is JSON form
+
+//     }
+//     else{
+//         // Fail
+//         console.log(err);
+//     }
+
+// });
+
+// request.get("https://jsonplaceholder.typicode.com/albums/51",function(err,response){
 //     if(err === null){
 //         //Successfully
 //         console.log(response);
@@ -48,17 +124,45 @@ const request = new Request();
 //         // Fail
 //         console.log(err);
 //     }
-    
+
 // });
 
-request.get("https://jsonplaceholder.typicode.com/albums/51",function(err,response){
-    if(err === null){
+// request.post("https://jsonplaceholder.typicode.com/albums",{userId:2,title:"Thriller"},function(err,album){
+
+//     if(err === null){
+//         console.log(album);
+//     }
+//     else{
+//         // Error
+//         console.log(err);
+
+//     }
+// });
+
+// request.put("https://jsonplaceholder.typicode.com/albums/10", { userId: 143, title: "Makar10" }, function (err, album) {
+
+//     if (err === null) {
+//         console.log(album);
+//     }
+//     else {
+//         // Error
+//         console.log(err);
+
+//     }
+// });
+
+request.delete("https://jsonplaceholder.typicode.com/albums/10", function (err, response) {
+    if (err === null) {
         //Successfully
         console.log(response);
     }
-    else{
+    else {
         // Fail
         console.log(err);
     }
-    
+
 });
+
+
+
+
