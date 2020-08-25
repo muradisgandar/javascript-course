92a41a0d1f10195fd3b981eaa49fac8a2eb21879
muradisgandar <isgandarli_murad@mail.ru>
2020-08-25 19:29:32 +0400

    components, props , css,bootstrap

--- udemy/Mustafa Murat Coshkun ReactJS Tutorial/reactapp/src/App.js
+++ udemy/Mustafa Murat Coshkun ReactJS Tutorial/reactapp/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import Navbar from "./components/Navbar"
+import User from "./components/User"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Navbar title="User App" />
+      <hr />
+
+      <User
+        name="Murad Isgandarli"
+        salary="5000"
+        department="Programming"
+
+      />
+
+      <User
+        name="Murad Isgandarli"
+        salary="5000"
+        department="Programming"
+
+      />
+
     </div>
   );
 }
--- udemy/Mustafa Murat Coshkun ReactJS Tutorial/reactapp/src/components/User.js
+++ udemy/Mustafa Murat Coshkun ReactJS Tutorial/reactapp/src/components/User.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 
 class User extends Component {
     render() {
+
+        // Destructing from JS
+        const {name,department,salary} = this.props;
+
         return (
             <div>
-                <form>
-                    <input type="text"/>
-                    <button>Send</button>
-                </form>
+                <ul>
+                    <li>Name : {name}</li>
+                    <li>Salary : {salary}</li>
+                    <li>Department : {department}</li>
+                </ul>
             </div>
         )
     }
