d7f17ca20bb124ba0c8449931194938c9c4a7a8b
muradisgandar <isgandarli_murad@mail.ru>
2020-08-25 23:19:15 +0400

    prop types and default props

--- udemy/Mustafa Murat Coshkun ReactJS Tutorial/reactapp/src/components/User.js
+++ udemy/Mustafa Murat Coshkun ReactJS Tutorial/reactapp/src/components/User.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 class User extends Component {
+
+    //same as below User.defaultProps block
+    static defaultProps = {
+        name: "Murad",
+        salary: 4000,
+        department: "Engineering"
+
+    }
+
+    //same as below User.propTypes block
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        salary: PropTypes.number.isRequired,
+        department: PropTypes.string.isRequired
+    }
+    
+
     render() {
 
         // Destructing from JS
-        const {name,department,salary} = this.props;
+        const { name, department, salary } = this.props;
 
         return (
             <div>
@@ -17,5 +36,17 @@ class User extends Component {
         )
     }
 }
+
+// User.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     salary: PropTypes.number.isRequired,
+//     department: PropTypes.string.isRequired
+// }
+
+// User.defaultProps = {
+//     name: "Murad",
+//     salary: 4000,
+//     department: "Programming"
+// }
 
 export default User;
