08587d252e83ed8ad243151b4e07f5e62444c958
muradisgandar <isgandarli_murad@mail.ru>
2020-10-09 15:35:15 +0400

    fix in settimeout and setinterval

--- udemy/Mustafa Murat Coshkun/Asynchronous programming , AJAX , before ES6/SetTimeOut and SetInterval/app.js
+++ udemy/Mustafa Murat Coshkun/Asynchronous programming , AJAX , before ES6/SetTimeOut and SetInterval/app.js
@@ -10,7 +10,7 @@ let value = setInterval(function(){
     i++;
     console.log("Number:",i);
 
-});
+},1000);
 document.getElementById("btn").addEventListener("click",function(){
     clearInterval(value);
 });
